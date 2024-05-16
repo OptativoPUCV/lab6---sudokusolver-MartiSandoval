84c68cf239cf54b39de231432ebb36211de6df8a
Your Name <you@example.com>
2024-05-16 03:15:27 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -58,8 +58,8 @@ List* get_adj_nodes(Node* n){
         // Si la celda está vacía, es un posible movimiento
         if(n->sudo[i][j] == 0) {
           for (int k = 1; k <= 9; k++) {
-            Node* newNode = createNode();
-            newNode = copy(n);
+             //newNode = createNode();
+            Node* newNode = copy(n);
             n->sudo[i][j] = k;
             pushBack(list, newNode);
           }
